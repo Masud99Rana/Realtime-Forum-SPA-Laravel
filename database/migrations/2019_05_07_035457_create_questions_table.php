df0c263917d0b732adf88d973268a639f35c1329
Masud99Rana <Masudrana917775@gmail.com>
2019-05-07 10:15:42 +0600

    model, migration complete

---
+++ database/migrations/2019_05_07_035457_create_questions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
